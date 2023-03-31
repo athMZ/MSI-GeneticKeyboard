c049070f8ea369840066debd5141a317af68a411
athMZ <s59091@student.ath.edu.pl>
2023-03-31 14:01:20 +0200

    Start of the project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSI_GeneticKeyboard
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Start of the project");
+        }
+    }
+}
